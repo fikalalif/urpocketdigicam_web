9ef4f97c2a0625e5330fc7ffd27ea787afa1e253
fikalalif <fikalgans26@gmail.com>
2025-10-11 21:33:47 +0700

    Force clear config cache on Vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,12 @@
 <?php
+
+// Paksa untuk membersihkan cache konfigurasi
+// Ini adalah langkah "brute force" untuk memastikan konfigurasi baru dibaca
+if (isset($_ENV['VERCEL'])) {
+    // Hapus file cache konfigurasi jika ada
+    if (file_exists(__DIR__ . '/../bootstrap/cache/config.php')) {
+        unlink(__DIR__ . '/../bootstrap/cache/config.php');
+    }
+}
+
 require __DIR__ . '/../public/index.php';
