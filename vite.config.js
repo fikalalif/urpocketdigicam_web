ddee2e5bbd1c1d1777a0a1f213f0631c63604868
fikalalif <fikalgans26@gmail.com>
2025-10-13 08:49:16 +0700

    fix: move vite build to api/public/build for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,32 @@
-import { defineConfig } from "vite";
+import {
+    defineConfig
+} from "vite";
 import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ["resources/css/app.css", "resources/js/app.js"],
-      refresh: true,
-    }),
-    tailwindcss(),
-  ],
-  build: {
-    manifest: "manifest.json", // 👈 tambahkan ini!
-    outDir: "public/build",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: ["resources/css/app.css", "resources/js/app.js"],
+    plugins: [
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+    build: {
+        outDir: 'api/public/build',
+        manifest: 'manifest.json',
+        rollupOptions: {
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+        },
     },
-  },
-  server: {
-    cors: true,
-  },
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "./"),
+
+    server: {
+        cors: true,
     },
-  },
+    resolve: {
+        alias: {
+            "~": path.resolve(__dirname, "./"),
+        },
+    },
 });
